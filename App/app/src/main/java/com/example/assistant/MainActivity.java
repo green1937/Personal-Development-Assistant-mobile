984fe279811efacbc5e03bf60f7cda7ca77fbcac
Sofia <sofiy.gavrilenko@gmail.com>
2024-10-03 11:10:48 +0700

    first commit

---
+++ App/app/src/main/java/com/example/assistant/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.assistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
