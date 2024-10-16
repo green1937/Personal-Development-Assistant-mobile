776a8b50e4bea8b0c236d1536c2919efcf66277a
Sofia <sofiy.gavrilenko@gmail.com>
2024-10-16 11:03:44 +0700

    добавлена проверка вводимых значений при создании задачи

--- App/app/src/main/java/com/example/assistant/NewTaskActivity.java
+++ App/app/src/main/java/com/example/assistant/NewTaskActivity.java
@@ -1,16 +1,25 @@
 package com.example.assistant;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class NewTaskActivity extends AppCompatActivity {
+    SimpleDateFormat sdfDATE = new SimpleDateFormat("dd.MM.yy", Locale.getDefault());
+    SimpleDateFormat sdfTIME = new SimpleDateFormat("HH:mm", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +59,79 @@ public class NewTaskActivity extends AppCompatActivity {
         2. В противном случае, задача не сохраняется и пользователь получает сообщение
            об ошибке сохранения.
 
-
-        ! Пока реализован только переход.
      */
     protected void saveTask() {
+
+        EditText nameTask = findViewById(R.id.taskName);          // название задачи
+        EditText scoreEditText = findViewById(R.id.scoreEditText);    // оценка
+        EditText dateFrom = findViewById(R.id.dateTimeTaskFromDate);  // дата от
+        EditText timeFrom = findViewById(R.id.dateTimeTaskFromTime);  // время от
+        EditText dateTo = findViewById(R.id.dateTimeTaskToDate);      // дата до
+        EditText timeTo = findViewById(R.id.dateTimeTaskToTime);      // время до
+
         ImageButton saveTaskBtn = findViewById(R.id.tickBtn);
         saveTaskBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(NewTaskActivity.this, MainActivity.class));
+                String taskNameStr = nameTask.getText().toString();
+                String scoreStr = scoreEditText.getText().toString();
+
+                String dateToStr = dateTo.getText().toString();
+                String timeToStr = timeTo.getText().toString();
+                String dateFromStr = dateFrom.getText().toString();
+                String timeFromStr = timeFrom.getText().toString();
+
+                // Оценка не может быть пустой и нельзя указывать только время без даты
+                if (taskNameStr.equals("") || scoreStr.equals("") || (dateToStr.equals("")  && !timeToStr.equals("")) || (dateFromStr.equals("")  && !timeFromStr.equals(""))) {
+                    Toast.makeText(getApplicationContext(), "Ошибка сохранения! Поля не могут быть пустыми!", Toast.LENGTH_SHORT).show();
+                }
+
+
+                else {
+                    int score = Integer.parseInt(scoreEditText.getText().toString());
+
+                    /*
+                        Если оценка НЕ в диапазоне от 1 до 100 и введенные даты
+                        и время НЕкорректных типов, то выводиться ошибка и задача не сохранится,
+                        пока ошибки не будут исправлены.
+
+                     */
+                    if (score<=0 || score > 100
+                            || checkDateTimeFormat(dateFromStr, "date") == 1
+                            || checkDateTimeFormat(dateToStr, "date") == 1
+                            || checkDateTimeFormat(timeFromStr, "time") == 1
+                            || checkDateTimeFormat(timeToStr, "time") == 1) {
+                        Toast.makeText(getApplicationContext(), "Ошибка сохранения!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        if ((dateFromStr.length() != 10 &&  checkDateTimeFormat(dateFromStr, "date") == 0)
+                                || (dateToStr.length() != 10 && checkDateTimeFormat(dateToStr, "date") == 0)) {
+                            Toast.makeText(getApplicationContext(), "Ошибка! Формат даты должен быть ДД.ММ.ГГГГ", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            // Если введены были обе даты
+                            if (checkDateTimeFormat(dateFromStr, "date") == 0 && checkDateTimeFormat(dateToStr, "date") == 0) {
+                                DateTimeFormatter dtfDATE = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.getDefault());
+                                LocalDate dateFromLD = LocalDate.parse(dateFromStr, dtfDATE);
+                                LocalDate dateToLD = LocalDate.parse(dateToStr, dtfDATE);
+
+                                // Проверка на то, что дата ОТ наступает раньше даты ДО
+                                if (dateFromLD.isAfter(dateToLD)) {
+                                    Toast.makeText(getApplicationContext(), "Ошибка сохранения! " +
+                                                    "Дата ОТ должна наступать раньше даты ДО!",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(NewTaskActivity.this, MainActivity.class));
+                                }
+                            } else {
+                                startActivity(new Intent(NewTaskActivity.this, MainActivity.class));
+                            }
+                        }
+
+
+                    }
+                }
+
             }
         });
     }
@@ -72,6 +145,35 @@ public class NewTaskActivity extends AppCompatActivity {
                 startActivity(new Intent(NewTaskActivity.this, RepeatTaskSettActivity.class));
             }
         });
+    }
 
+    protected int checkDateTimeFormat(String param1, String param2) {
+        int flag = 0;
+        if (!param1.equals("")) {
+            if (param2.equals("date")) {
+                sdfDATE.setLenient(false);
+                try {
+                    sdfDATE.parse(param1);
+                    System.out.println("Valid date");
+                } catch (ParseException e) {
+                    System.out.println("Invalid date");
+                    flag = 1;
+                }
+            }
+            if (param2.equals("time")) {
+                sdfTIME.setLenient(false);
+                try {
+                    sdfTIME.parse(param1);
+                    System.out.println("Valid time");
+                } catch (ParseException e) {
+                    System.out.println("Invalid time");
+                    flag = 1;
+                }
+            }
+        }
+        else {
+            flag = 2;
+        }
+        return flag;
     }
 }
